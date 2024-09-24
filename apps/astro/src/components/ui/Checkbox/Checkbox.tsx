aafafe82fae93424164a26730e1273bb4504d9b7
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-09-24 16:44:45 +0200

    add Checkbox with TSX

---
+++ apps/astro/src/components/ui/Checkbox/Checkbox.tsx
@@ -0,0 +1,26 @@
+import Error from '@/src/components/ui/Error'
+import styles from './styles.module.scss'
+import type { FieldErrors } from 'react-hook-form';
+
+type Props = {
+  register: {
+    name: string
+  }
+  children: React.ReactNode
+  errors: FieldErrors;
+} & React.HTMLAttributes<HTMLLabelElement>
+
+export default function Checkbox({ children, register, errors, ...props }: Props) {
+  return (
+    <label className={styles.label} {...props}>
+      <div className={styles.checkmark}>
+        <input type="checkbox" {...register} aria-invalid={!!errors[register.name]} />
+        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="11" fill="none">
+          <path stroke="#DBB039" stroke-width="2" d="M.987 4.974 5.013 9l8-8"></path>
+        </svg>
+      </div>
+      <p>{children}</p>
+      <Error error={errors[register.name]?.message?.toString()} />
+    </label>
+  )
+}
