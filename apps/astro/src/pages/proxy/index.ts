6cb94557d72e45004b8d66da5a3740461728779b
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2024-09-26 13:08:57 +0200

    simplify the proxy return

--- apps/astro/src/pages/proxy/index.ts
+++ apps/astro/src/pages/proxy/index.ts
@@ -6,21 +6,15 @@ export const GET: APIRoute = async ({ request }) => {
   const url = new URL(request.url);
   const targetUrl = url.searchParams.get('url');
 
-
   if (!targetUrl) {
     return new Response('Missing URL parameter', { status: 400 });
   }
 
   try {
     const response = await fetch(targetUrl, {
-      headers: {
-        ...request.headers,
-        host: new URL(targetUrl).host,
-      },
+      headers: request.headers,
     });
-    const headers = new Headers(response.headers);
-    headers.set('Access-Control-Allow-Origin', '*');
-    return new Response(response.body, { headers });
+    return new Response(response.body)
   } catch (error) {
     console.error('Proxy error:', error);
     return new Response('Proxy error', { status: 500 });
