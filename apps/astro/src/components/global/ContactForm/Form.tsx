2b70bf3eb41269d96d41d42f837ba0007e7100dd
Bogumił <47248711+milewskibogumil@users.noreply.github.com>
2025-04-15 16:41:30 +0200

    add formInfo about catalog and platform

--- apps/astro/src/components/global/ContactForm/Form.tsx
+++ apps/astro/src/components/global/ContactForm/Form.tsx
@@ -111,7 +111,14 @@ export default function Form({ analytics }: Props) {
           value={contactType}
         />
       </div>
-
+      {contactType === 'email' && (
+        <div className={styles.formInfo}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <path d="M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H20v20H6.5a2.5 2.5 0 0 1 0-5H20"></path>
+          </svg>
+          <p>Po uzupełnieniu formularza wyślemy Ci nasz katalog oraz link do platformy, gdzie w 2 minuty uzyskasz wycenę swojego projektu.</p>
+        </div>
+      )}
       <div
         role="tabpanel"
         id={contactType === 'email' ? 'contact-email-panel' : 'contact-phone-panel'}
